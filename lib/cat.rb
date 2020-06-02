462c9d36e6f1ebc35fc72af510041112ecfdd9f4
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-06-02 10:40:12 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name,owner)
+    @name, @owner = name, owner
+    @mood = "nervous" #returns nil if otherwise
+    @@all << self
+  end 
+
+  def self.all 
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
 class Owner
-  # code goes here
+  attr_accessor :buy_cat, :pets
+  attr_reader :name, :species, :say_species
+  #making .name an attr_reader let's it remain unchanged. 
+
+  @@all = []
+  
+
+  def initialize(name)
+    @name = name 
+    @species = "human" #returns nil if otherwise
+    @say_species = "I am a #{self.species}."
+    @pets = {:dogs => [], :cats => []}
+    @@all << self
+  end 
+
+  def self.all 
+    @@all 
+  end
+
+  def self.count 
+    @@count = @@all.length
+  end
+
+  def self.reset_all 
+    @@all.clear
+  end
+
+  def cats 
+    Cat.all.select {|cats| cats.owner == self}
+  end
+
+  def dogs 
+    Dog.all.select {|dogs| dogs.owner == self}
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)#knows about their cat
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self) #knows about their dog
+  end
+
+  def walk_dogs 
+    Dog.all.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats 
+    Cat.all.each do |cat| 
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets 
+    @pets.collect do |instances|
+      instances.each do |pet|
+        pets.mood = "nervous"
+      end
+      instances.clear
+    end
+  end
+
 end
