6f51927f638d31d818557e55b5b04043cf2d04ac
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-06-03 18:35:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,19 @@
 class Dog
   
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
   
   def initialize(name, owner)
     @name = name
-    @owner = Owner.new(owner)
-    
-    
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  
+  def self.all 
+    @@all 
   end
 end
