bbafd637740f45636248a089d3b4587b6ec3e92a
Jack Quinoa <jquinoa208@gmail.com>
2020-06-08 22:58:48 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  
+  attr_reader :name 
+  @@all = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name 
+    @mood = mood 
+    @owner = owner 
+    @@all << self 
+  end
+ 
+   
+  def self.all 
+    @@all 
+  end
+  
 end
